aa8ffe4db8caf69ccc2789db1cac12f9c1ada303
Yuliia <julia.gancheva1990@gmail.com>
2020-04-02 15:23:38 +0300

    components, functions were modified

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,56 +1,90 @@
 import React from 'react';
 
+function calculateBMI(height, weight) {
+  let squareMeters = Math.pow(height, 2)
 
-function Range(props) {
+  return (weight / (squareMeters / 100) * 100).toFixed(1)
+}
+function reportBMI(height, weight) {
+  let bmi = calculateBMI(height, weight)
+
+  if (bmi < 18.5) {
+    return `${bmi} Underweight`
+  } else if(bmi >= 18.5 && bmi <= 24.9) {
+    return `${bmi} Normal weight`
+  } else if(bmi >= 25 && bmi <= 29.9) {
+    return `${bmi} Overweight`
+  } else if(bmi >= 30) {
+    return `${bmi} Obesity`
+  }
+}
+function Range({label, min, max, value, onChange}) {
+
   return <div className="slide-container">
-    <label htmlFor="bmi-slider" className="slider-title">{props.label}</label>
-    <span className="min-num">{props.min}</span>
-    <input type="range" min={props.min} max={props.max} step="1" className="slider"
-           onChange={props.onChange} value={props.value} id="bmi-slider" />
-    <span className="max-num">{props.max}</span>
+    <label htmlFor="bmi-slider" className="slider-title">
+      {label}
+    </label>
+    <span className="min-num">
+      {min}
+    </span>
+    <input type="range" id="bmi-slider"
+           className="slider"
+           min={min} max={max} step="1"
+           onChange={onChange} value={value} />
+    <span className="max-num">
+      {max}
+    </span>
   </div>
 }
 
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {weight: 35, height: 90};
-    this.handleWeightChange = this.handleWeightChange.bind(this);
-    this.handleHeightChange = this.handleHeightChange.bind(this);
+  state = {
+    weight: 35,
+    height: 90,
   }
+
   handleWeightChange(e) {
     this.setState({ weight: e.target.value});
   }
+
   handleHeightChange(e) {
     this.setState({ height: e.target.value});
   }
 
-  calculate() {
-    let s = Math.pow(this.state.height, 2);
-    let num = (this.state.weight / (s / 100) * 100).toFixed(1);
-    if(num < 18.5) {
-      return `${num} Underweight`;
-    } else if(num >= 18.5 && num <= 24.9) {
-      return `${num} Normal weight`;
-    } else if(num >= 25 && num <= 29.9) {
-      return `${num} Overweight`;
-    }else if(num >= 30) {
-      return `${num} Obesity`;
-    }
-  }
-
   render() {
+    let {height, weight} = this.state
+    let bmi = reportBMI(height, weight)
+
     return (
       <div className="calculator-container">
         <h2 className="title">BMI calculator</h2>
-        <Range value={this.state.height} min="90" max="245" label="Height"
-               onChange = {this.handleHeightChange} />
-        <div className="height">{`${this.state.height} cm`}</div>
 
-        <Range value={this.state.weight} min="35" max="200" label="Weight"
-               onChange = {this.handleWeightChange} />
-        <div className="weight">{`${this.state.weight} kg`}</div>
-        <div className="result"><span>BMI</span>{this.calculate()}</div>
+        <Range
+          value={height}
+          min="90" max="245"
+          label="Height"
+          onChange={(e) => this.handleHeightChange(e)}
+        />
+
+        <div className="height">
+          {height} cm
+        </div>
+
+        <Range
+          value={weight}
+          min="35" max="200"
+          label="Weight"
+          onChange={(e) => this.handleWeightChange(e)}
+        />
+
+        <div className="weight">
+          {weight} kg
+        </div>
+
+        <div className="result">
+          <span>BMI</span>
+          {bmi}
+        </div>
       </div>
     )
   }
